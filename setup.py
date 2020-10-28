cc92746277dc3700425063f6cfa8fdea4684f2f2
adilson <adilson@zookal.com>
2020-10-28 14:40:28 +1100

    new release

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@
 from setuptools import setup
 
 setup(name='tap-auth0',
-      version='0.0.1',
+      version='0.1.0',
       description='Singer.io tap for extracting data from the Auth0 Management API v2',
-      author='Jean-Nicholas Hould (jeannicholashould.com)',
+      author='Adilson Mendonca based on Jean-Nicholas Hould',
       url='http://singer.io',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
       py_modules=['tap_auth0'],
@@ -19,9 +19,7 @@ setup(name='tap-auth0',
       ''',
       packages=['tap_auth0'],
       package_data = {
-          'tap_auth0/schemas': [
-              "users.json",
-          ],
+          'tap_auth0': ["schemas/*.json"],
       },
       include_package_data=True,
 )
